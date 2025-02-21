ba63e517ec3096a40d2253dad3bdd5cc8ac5d5a0
Barder_Lucas <bardey.estelle1@gmail.com>
2025-02-21 12:00:01 +0100

    Mise à jour de la configuration de PocketBase pour utiliser l'URL d'environnement

--- src/backend.ts
+++ src/backend.ts
@@ -1,4 +1,4 @@
 import Pocketbase from 'pocketbase'
 import type { TypedPocketBase } from './pocketbase-typegen'
 
-export const pb = new Pocketbase('http://127.0.0.1:8090/_/') as TypedPocketBase;
+export const pb = new Pocketbase(import.meta.env.VITE_URL_POCKETBASE) as TypedPocketBase;
